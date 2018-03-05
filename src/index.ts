d651cc2332354daff661e7c7618d530eabce40d6
Chicloon@gmail.com <gfhjkm1982>
2018-03-05 21:56:04 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import "reflect-metadata";
 import { GraphQLServer } from "graphql-yoga";
-import { createConnection } from "typeorm";
+import { createConnection, getConnection } from "typeorm";
 import resolvers from "./resolvers";
+
+import { User } from "./entity/User";
+
 // const typeDefs = `
 //   type Query {
 //     hello(name: String): String!
@@ -10,9 +13,37 @@ import resolvers from "./resolvers";
 
 // const resolvers = {};
 
+async function getUser(req: any) {
+  // try {
+  // console.log(req);
+  // console.log("==========headers", req.headers);
+  const token = (await req.headers.auth) ? req.headers.auth : null;
+  // console.log(req.headers);
+  console.log(token);
+  const user = await getConnection()
+    .getRepository(User)
+    .findOneById(token);
+  console.log(user);
+  // if (token != null) {
+  //   user = await decodeToken(token);
+  //   req.user = user;
+  // } else {
+  //   req.user = null;
+  // }
+  return user;
+  // } catch (error) {
+  //   throw error;
+  // }
+}
+
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: async req => ({
+    user: await getUser(req.request)
+    // request: req.request.headers
+    // theContext: req
+  })
 });
 createConnection().then(() => {
   server.start(() => console.log("Server is running on localhost:4000"));
--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -3,7 +3,9 @@ import { User } from "../entity/User";
 
 export const Query = {
   hello: (_: any, { name }: any) => `hhello ${name || "World"}`,
-  users: () => {
+  users: (_: any, args: any, context: any) => {
+    console.log("context====", context);
+    console.log("====args", args);
     return getConnection()
       .getRepository(User)
       .find();
